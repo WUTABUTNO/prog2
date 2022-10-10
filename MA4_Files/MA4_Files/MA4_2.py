c607cb4406c62952577441b9bc8841208f115f18
William Samuelsson <wisa7741@arrhenius.it.uu.se>
2022-10-10 09:38:48 +0200

    added png

--- MA4_Files/MA4_Files/MA4_2.py
+++ MA4_Files/MA4_Files/MA4_2.py
@@ -1,12 +1,55 @@
 #!/usr/bin/env python3.9
 
 from person import Person
+from time import perf_counter as pc
+from numba import njit
+import matplotlib.pyplot as plt
+
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return fib_py(n-1) + fib_py(n-2)
+
+@njit
+def fib_numba(n):
+	if n <= 1:
+		return n
+	else:
+		return fib_numba(n-1) + fib_numba(n-2)
+
+
+
+
 
 def main():
 	f = Person(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
-
+	n = [nn for nn in range(30,46)]
+	pytimes = []
+	numbatimes = []
+	cpptimes = []
+	for nn in n:
+		print(nn)
+		f.set(nn)
+		print("Py")
+		t0 = pc()
+		fib_py(nn)
+		t1 = pc()
+		pytimes.append(t1-t0)
+		print("Numba")
+		t0 = pc()
+		fib_numba(nn)
+		t1 = pc()
+		numbatimes.append(t1-t0)
+		print("C++")
+		t0 = pc()
+		f.fib()
+		t1 = pc()
+		cpptimes.append(t1-t0)
+	plt.plot(n, pytimes, 'r.')
+	plt.plot(n, numbatimes, 'b.')
+	plt.plot(n, cpptimes, 'g.')
+	plt.savefig("FibCompTimes", facecolor='white', transparent=False) 
 if __name__ == '__main__':
 	main()
--- MA4_Files/MA4_Files/person.cpp
+++ MA4_Files/MA4_Files/person.cpp
@@ -1,36 +1,54 @@
 #include <cstdlib>
+#include <iostream>
 // Person class 
 
 class Person{
 	public:
 		Person(int);
+		int fib();
 		int get();
 		void set(int);
 	private:
 		int age;
-	};
+		int ffib(int);
+};
  
 Person::Person(int n){
+	std::cout << "initializing" << std::endl;
 	age = n;
 	}
- 
+
 int Person::get(){
 	return age;
 	}
- 
+
 void Person::set(int n){
 	age = n;
 	}
 
+int Person::ffib(int n) {
+        if (n <= 1){
+        return n;
+        	}
+        else{
+                return ffib(n-1) + ffib(n-2);
+        	}
+	}
+
+int Person::fib(){
+	return ffib(age);
+	}
 
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
-	void Person_delete(Person* person){
+	int Person_fib(Person* person) {return person->fib();}
+ 	void Person_delete(Person* person){
 		if (person){
 			delete person;
 			person = nullptr;
 			}
 		}
 	}
+
